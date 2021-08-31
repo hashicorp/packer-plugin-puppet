a12ee5ed34143cd52cfbfb0cb9d4929c57a4288b
Adrien Delorme <azr@users.noreply.github.com>
2021-08-31 15:56:16 +0200

    consolidate version location

--- main.go
+++ main.go
@@ -4,32 +4,18 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/hashicorp/packer-plugin-sdk/plugin"
+
 	puppetmasterlessProv "github.com/hashicorp/packer-plugin-puppet/provisioner/puppet-masterless"
 	puppetserverProv "github.com/hashicorp/packer-plugin-puppet/provisioner/puppet-server"
-
-	"github.com/hashicorp/packer-plugin-sdk/plugin"
-	"github.com/hashicorp/packer-plugin-sdk/version"
-)
-
-var (
-	// Version is the main version number that is being run at the moment.
-	Version = "1.0.0"
-
-	// VersionPrerelease is A pre-release marker for the Version. If this is ""
-	// (empty string) then it means that it is a final release. Otherwise, this
-	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = ""
-
-	// PluginVersion is used by the plugin set to allow Packer to recognize
-	// what version this plugin is.
-	PluginVersion = version.InitializePluginVersion(Version, VersionPrerelease)
+	"github.com/hashicorp/packer-plugin-puppet/version"
 )
 
 func main() {
 	pps := plugin.NewSet()
 	pps.RegisterProvisioner("masterless", new(puppetmasterlessProv.Provisioner))
 	pps.RegisterProvisioner("server", new(puppetserverProv.Provisioner))
-	pps.SetVersion(PluginVersion)
+	pps.SetVersion(version.PluginVersion)
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
